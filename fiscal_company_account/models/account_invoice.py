602713b129f72145f979757de88bb98f1d8568b7
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-01-20 12:15:12 +0100

    [FIX] fiscal_company_account : remove domain on journal_id field of model account.invoice, regarding company

--- fiscal_company_account/models/account_invoice.py
+++ fiscal_company_account/models/account_invoice.py
@@ -2,9 +2,21 @@
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models
+from odoo import fields, models
 
 
 class AccountInvoice(models.Model):
     _name = 'account.invoice'
     _inherit = ['account.invoice', 'fiscal.mother.check.mixin']
+
+    # Remove the company part of the domain
+    journal_id = fields.Many2one(
+        domain="""[
+            ('type', 'in', {
+                'out_invoice': ['sale'],
+                'out_refund': ['sale'],
+                'in_refund': ['purchase'],
+                'in_invoice': ['purchase']
+            }.get(type, []))
+        ]
+        """)
